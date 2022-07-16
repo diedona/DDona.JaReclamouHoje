8313c07ce83008eebdbde58e9005e1048357c788
Diego Doná <diedona@gmail.com>
2022-07-15 21:32:36 -0300

    criando entidade base testes

---
+++ DDona.JaReclamouHoje.Testes/Entities/EntidadeBaseTestes.cs
@@ -0,0 +1,22 @@
+﻿using DDona.JaReclamouHoje.Dominio.Entities;
+using DDona.JaReclamouHoje.Dominio.Entities.Base;
+
+namespace DDona.JaReclamouHoje.Testes.Entities
+{
+    public class EntidadeBaseTestes
+    {
+        [Fact]
+        public void Entidade_Criada_Deve_Ter_Id()
+        {
+            EntidadeBase entidade = new Pessoa("Diego", "diego.dona");
+            Assert.NotEqual(Guid.Empty, entidade.Id);
+        }
+
+        [Fact]
+        public void Entidade_Criada_Deve_Ter_DataCriacao()
+        {
+            EntidadeBase entidade = new Pessoa("Diego", "diego.dona");
+            Assert.NotEqual(DateTime.MinValue, entidade.DataCriacao);
+        }
+    }
+}
