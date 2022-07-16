e29850009314f77babc3906a6f7080cc3cafb31e
Diego Doná <diedona@gmail.com>
2022-07-16 19:28:31 -0300

    add api

---
+++ DDona.JaReclamouHoje.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDona.JaReclamouHoje.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("welcome home");
+        }
+    }
+}
---
+++ DDona.JaReclamouHoje.Api/Startup.cs
@@ -0,0 +1,35 @@
+﻿namespace DDona.JaReclamouHoje.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get;}
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
