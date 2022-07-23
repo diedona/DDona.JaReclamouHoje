1a118ea27c0e377d5a0a16021736923c264fa338
Diego Doná <diedona@gmail.com>
2022-07-23 19:03:18 -0300

    adicionando teste de reclamacao

---
+++ src/backend/DDona.JaReclamouHoje.Testes/Entities/ReclamacaoTestes.cs
@@ -0,0 +1,19 @@
+﻿using DDona.JaReclamouHoje.Dominio.Entities;
+
+namespace DDona.JaReclamouHoje.Testes.Entities
+{
+    public class ReclamacaoTestes
+    {
+        [Fact]
+        public void Deve_Adicionar_Complemento()
+        {
+            var pessoa = new Pessoa("Diego Doná", "diego.dona");
+            var outraPessoa = new Pessoa("Pedro Pedrinho", "pedro.pedrinho");
+            var assunto = new Assunto("Frontend");
+            var reclamacao = pessoa.CriarReclamacao(assunto, "Não gosto");
+            var complemento = outraPessoa.CriarComplemento(reclamacao, "Nem eu!");
+
+            Assert.Equal(1, reclamacao.Complementos.Count);
+        }
+    }
+}
