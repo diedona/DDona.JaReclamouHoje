15232bc682c31153438efe6968495464919acb46
Diego Doná <diedona@gmail.com>
2022-07-23 12:02:03 -0300

    teste com erro proposital

---
+++ src/backend/DDona.JaReclamouHoje.Testes/Entities/ComplementoTestes.cs
@@ -0,0 +1,11 @@
+﻿namespace DDona.JaReclamouHoje.Testes.Entities
+{
+    public class ComplementoTestes
+    {
+        [Fact]
+        public void Deve_Falhar()
+        {
+            Assert.False(true);
+        }
+    }
+}
